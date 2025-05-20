bc4034037a0e8b3fe7b5a2927cd7ba770dc4dcdf
navi <jha.navi007@gmail.com>
2025-05-21 01:18:08 +0530

    apig

--- APIGatewayMorningBatch-main/src/main/java/com/example/apigatewaymorningbatch/ApiGatewayMorningBatchApplication.java
+++ APIGatewayMorningBatch-main/src/main/java/com/example/apigatewaymorningbatch/ApiGatewayMorningBatchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayMorningBatchApplication {
-//apigateway
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayMorningBatchApplication.class, args);
     }
